3cc99e302a498646a7b7d5975e41a11a0d94cbc9
Mobin Kakar <mobin.kakar@wsabronx.org>
2020-09-03 19:58:22 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,15 +1,2 @@
 class Post
-
-  attr_accessor :title, :author
-  def initialize(title)
-    @title = title
-  end
-
-  def author_name
-    if @author == nil
-      return nil
-    else
-      @author.name
-    end
-  end
 end
